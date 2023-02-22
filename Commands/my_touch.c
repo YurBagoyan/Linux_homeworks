d14d4b08269c2ce46032b67290ae6cfc806c1b99
Yura Bagoyan <90376899+YurBagoyan@users.noreply.github.com>
2023-02-22 18:18:16 +0400

    My custom touch command

--- Commands/my_touch.c
+++ Commands/my_touch.c
@@ -5,10 +5,10 @@
 
 int isDir(char* lastArgument)
 {
-    struct stat path;
-    stat(lastArgument, &path);
+	struct stat path;
+    	stat(lastArgument, &path);
 
-    return S_ISREG(path.st_mode);
+    	return S_ISREG(path.st_mode);
 }
 
 char* getPath(char* path, int* fileNameCount)
@@ -17,42 +17,42 @@ char* getPath(char* path, int* fileNameCount)
         printf("Last one is directory. Files created in %s%s", path, " directory\n");
 		*fileNameCount = *fileNameCount - 1;
 		return path;		
-    } else {
-        printf("Last argument is not directory. Files created in current directory\n");
-        return "./";
-    }
+    	} else {
+        	printf("Last argument is not directory. Files created in current directory\n");
+        	return "./";
+    	}
 }
 
 void touch(char* filePath)
 {
-    struct stat st;
+    	struct stat st;
 
-    if (stat(filePath, &st) == 0) {
+    	if (stat(filePath, &st) == 0) {
 		/* File exists, update access and modification times */
-        utime(filePath, NULL);
-    } else {
-        int fd = creat(filePath, 0666);
-        if(fd < 0) {
-            printf("Can't create file: %s%s", filePath,  "\n");
-        } 
-    }   
+        	utime(filePath, NULL);
+    	} else {
+        	int fd = creat(filePath, 0666);
+        	if(fd < 0) {
+            		printf("Can't create file: %s%s", filePath,  "\n");
+        	} 
+    	}   
 }
 
 int main(int argc, char* argv[])
 {
-    if(argc < 2) {
-        printf("Invalid options provided\n");
-        return -1;
-    }
+	if(argc < 2) {
+        	printf("Invalid options provided\n");
+        	return -1;
+    	}
 
 	int fileNameCount = argc;
-    char* path = getPath(argv[argc-1], &fileNameCount);  
+    	char* path = getPath(argv[argc-1], &fileNameCount);  
 
-    for(int i = 1; i < fileNameCount; ++i) {
+    	for(int i = 1; i < fileNameCount; ++i) {
 		char fullPath[4095];		
-        snprintf(fullPath, sizeof(fullPath), "%s/%s", path, argv[i]);
-        touch(fullPath);
-    }  
+        	snprintf(fullPath, sizeof(fullPath), "%s/%s", path, argv[i]);
+        	touch(fullPath);
+    	}  
 
-    return 0;
+    	return 0;
 }
