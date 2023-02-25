cffe7f7572132ab93efed6393504c2a118997789
Yura Bagoyan <yurbagoyan@gmail.com>
2023-02-25 15:26:57 +0400

    My custom touch command

--- Commands/my_touch.c
+++ Commands/my_touch.c
@@ -17,42 +17,45 @@ char* getPath(char* path, int* fileNameCount)
         printf("Last one is directory. Files created in %s%s", path, " directory\n");
 		*fileNameCount = *fileNameCount - 1;
 		return path;		
-    	} else {
-        	printf("Last argument is not directory. Files created in current directory\n");
-        	return "./";
-    	}
+    
+	} else {
+		printf("Last argument is not directory. Files created in current directory\n");
+        return "./";
+    }
 }
 
 void touch(char* filePath)
 {
-    	struct stat st;
+    struct stat st;
 
-    	if (stat(filePath, &st) == 0) {
-		/* File exists, update access and modification times */
-        	utime(filePath, NULL);
-    	} else {
-        	int fd = creat(filePath, 0666);
-        	if(fd < 0) {
-            		printf("Can't create file: %s%s", filePath,  "\n");
-        	} 
-    	}   
+	if (stat(filePath, &st) == 0) {
+	/* File exists, update access and modification times */
+		utime(filePath, NULL);
+    
+	} else {
+		int fd = creat(filePath, 0666);
+        
+		if(fd < 0) {
+        printf("Can't create file: %s%s", filePath,  "\n");
+        } 
+    }   
 }
 
 int main(int argc, char* argv[])
 {
 	if(argc < 2) {
-        	printf("Invalid options provided\n");
-        	return -1;
-    	}
+       	printf("Invalid options provided\n");
+       	return -1;
+   	}
 
 	int fileNameCount = argc;
-    	char* path = getPath(argv[argc-1], &fileNameCount);  
+   	char* path = getPath(argv[argc-1], &fileNameCount);  
 
-    	for(int i = 1; i < fileNameCount; ++i) {
-		char fullPath[4095];		
-        	snprintf(fullPath, sizeof(fullPath), "%s/%s", path, argv[i]);
-        	touch(fullPath);
-    	}  
+    for(int i = 1; i < fileNameCount; ++i) {
+	char fullPath[4095];		
+       	snprintf(fullPath, sizeof(fullPath), "%s/%s", path, argv[i]);
+       	touch(fullPath);
+    }  
 
-    	return 0;
+   	return 0;
 }
