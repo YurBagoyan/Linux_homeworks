8f84ba51a1b057dae698b9c69d7dab5bbae80445
YurBagoyan <yurbagoyan@gmail.com>
2023-03-29 10:07:23 +0400

    mv command fixing

--- Commands/my_mv.c
+++ Commands/my_mv.c
@@ -16,38 +16,37 @@ int isDir(char* argument)
 	return S_ISDIR(path.st_mode);
 }
 
-char* getFileNameFromPath(char* filePath)
-{	
-	char temp[100] ;
-	strncpy(temp, filePath, 100);
-	
-	char* by = "/";	
-	char* name;
-	
-	char* ptr = strtok(temp, by);
-	while (ptr != NULL) {
-		name = ptr;
-		ptr = strtok(NULL, by);
-	}
-	
-	return name;
+char* get_file_name(char* path) {
+    // Find the last occurrence of '/' or '\' in the path
+    char* last_slash = strrchr(path, '/');
+    if (!last_slash) {
+        last_slash = strrchr(path, '\\');
+    }
+    if (!last_slash) {
+        // No slash found, so return the original path
+        return path;
+    } else {
+        // Return the substring after the last slash
+        return last_slash + 1;
+    }
 }
 
 void createAndCopy(char* newFile, const int source_fd)
 {
-	int fd = creat(newFile, 0666);
-	if(fd < 0) {
+	// Create new file
+	int dest_fd = creat(newFile, 0666);
+	if(dest_fd < 0) {
 		printf("File not created\n ");
 		exit(-1);
 	}
-		
+	
+	// Copy data
 	char buf[1];
 	int i = 0;
 	while((i = read(source_fd, buf, 1)) > 0) {        
-		write(fd, buf, i);
+		write(dest_fd, buf, i);
 	}
 }
-
 
 int main(int argc, char* argv[])
 {
@@ -56,29 +55,27 @@ int main(int argc, char* argv[])
         exit(-1);
 	}
 	
-	char* movedFile = argv[1];
+	char* sourceFile = argv[1];
 	char* newFile = argv[2];
 	
-	int source_fd = open(movedFile, O_RDONLY);
-		if(source_fd < 0) {
-			printf("File not opened\n ");
-			exit(-1);
+	int source_fd = open(sourceFile, O_RDONLY);
+	if(source_fd < 0) {
+		printf("File not opened\n ");	
+		exit(-1);
 	}	
 
-	if(!isDir(newFile)) {		
-		createAndCopy(newFile, source_fd);
-	} else {
-		char* movedFileName = getFileNameFromPath(movedFile);
+	if(isDir(newFile)) {		
+		char* sourceFileName = get_file_name(sourceFile);
 		strcat(newFile, "/");
-		strcat(newFile, movedFileName);
-		
-		createAndCopy(newFile, source_fd);
+		strcat(newFile, sourceFileName);
 	}
 	
+	createAndCopy(newFile, source_fd);
+	
 	close(source_fd);
-	int r = remove(movedFile);
+	int r = remove(sourceFile);
 	if (r < 0) {
-		printf("%s File is not removed\n", movedFile);
+		printf("%s File is not removed\n", sourceFile);
 	}
 	
 	return 0;
