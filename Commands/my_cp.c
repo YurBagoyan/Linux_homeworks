b989d7b46292fac8d82a89a2e2668cb44466a939
YurBagoyan <yurbagoyan@gmail.com>
2023-03-29 09:44:37 +0400

    cp command fixing

--- Commands/my_cp.c
+++ Commands/my_cp.c
@@ -6,24 +6,20 @@
 #include <utime.h> // utime
 #include <string.h> // strncpy()
 
-
-char* getCopiedFileName(char* copiedFilePath)
-{	
-	char temp[100] ;
-	strncpy(temp, copiedFilePath, 100);
-	
-	char* by = "/";	
-	char* name;
-	
-	char* ptr = strtok(temp, by);
-	while (ptr != NULL) {
-		name = ptr;
-		ptr = strtok(NULL, by);
-	}
-	
-	return name;
+char* get_file_name(char* path) {
+    // Find the last occurrence of '/' or '\' in the path
+    char* last_slash = strrchr(path, '/');
+    if (!last_slash) {
+        last_slash = strrchr(path, '\\');
+    }
+    if (!last_slash) {
+        // No slash found, so return the original path
+        return path;
+    } else {
+        // Return the substring after the last slash
+        return last_slash + 1;
+    }
 }
-
 
 int main(int argc, char* argv[])
 {
@@ -32,31 +28,32 @@ int main(int argc, char* argv[])
         exit(-1);
     }
 	
-	char* copiedFilePath = argv[1];
+	char* sourceFilePath = argv[1];
 	char* newFilePath = argv[2];		
-	char* copiedFileName = getCopiedFileName(copiedFilePath);	
+	char* sourceFileName = get_file_name(sourceFilePath);	
 	
 	char fullPathOfNewFile[4095];
-	snprintf(fullPathOfNewFile, sizeof(fullPathOfNewFile), "%s/%s", newFilePath, copiedFileName);
+	snprintf(fullPathOfNewFile, sizeof(fullPathOfNewFile), "%s/%s", newFilePath, sourceFileName);
 	
-	int fd = creat(fullPathOfNewFile, 0666);
-    if(fd < 0) {
+	// Create new file
+	int dest_fd = creat(fullPathOfNewFile, 0666);
+    if(dest_fd < 0) {
         printf("File not created\n ");
 		exit(-1);
     }
 	
-	int source_fd = open(copiedFilePath, O_RDONLY);
+	// Open source file
+	int source_fd = open(sourceFilePath, O_RDONLY);
 	 if(source_fd < 0){
         printf("File not opened\n ");
 		exit(-1);
     }
 	
-	
+	// Copy data
 	char buf[1];
 	int i = 0;
-	
     while((i = read(source_fd, buf, 1)) > 0) {        
-        write(fd, buf, i);
+        write(dest_fd, buf, i);
 	}
 	
 	return 0;
